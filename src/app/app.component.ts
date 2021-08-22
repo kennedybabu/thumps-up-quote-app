0081dafde877e1ab0fa55792f3c84d5216870665
kennedy babu <kennedynjogu8@gmail.com>
2021-08-22 06:38:31 +0300

    generate the Quote interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from "./quote"
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'quote-gen';
+
+
+  quotes:Quote[] = [
+    {id:1, quote: 'wash utensils', author: "theGreat", submittedBy:"manHasNoName" },
+    {id:2, quote: 'hulla ballooo', author: "alexander", submittedBy:"collo" },
+    {id:3, quote: 'wash the cat', author: "noOne", submittedBy:"whiskers" },
+    {id:4, quote: 'get cat food', author: "mimi", submittedBy:"humbleMan" }
+  ]
+
 }
+
---
+++ src/app/quote.ts
@@ -0,0 +1,6 @@
+export interface Quote {
+    id: number;
+    quote: string;
+    author: string;
+    submittedBy: string
+}
