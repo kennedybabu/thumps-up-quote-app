ba702fd9ab8947c42fac19f50127e3b7a8c8ea74
kennedy babu <kennedynjogu8@gmail.com>
2021-08-22 06:45:53 +0300

    nest the quote component in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,5 @@ import { Quote } from "./quote"
 })
 export class AppComponent {
 
-
-  quotes:Quote[] = [
-    {id:1, quote: 'wash utensils', author: "theGreat", submittedBy:"manHasNoName" },
-    {id:2, quote: 'hulla ballooo', author: "alexander", submittedBy:"collo" },
-    {id:3, quote: 'wash the cat', author: "noOne", submittedBy:"whiskers" },
-    {id:4, quote: 'get cat food', author: "mimi", submittedBy:"humbleMan" }
-  ]
-
 }
 
