7e1a5080f51bfa75af49ec66f8234c3ddd3355a1
kennedy babu <kennedynjogu8@gmail.com>
2021-08-23 08:26:19 +0300

    change the backgrounds path and hard coded quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,10 +10,10 @@ export class QuoteComponent implements OnInit {
 
 
   quotes: Quote[] = [
-    new Quote(1,  'wash utensils', "theGreat", "manHasNoName", false),
-    new Quote(2,  'hulla ballooo', "alexander", "collo", false ),
-    new Quote(3,  'wash the cat', "noOne", "whiskers" ,false ),
-    new Quote(4,  'get cat food', "mimi", "humbleMan" , false)
+    new Quote(1,  'Every one of us is, in the cosmic perspective, precious. If a human disagrees with you, let him live. In a hundred billion galaxies, you will not find another.', "Carl Sagan", "manHasNoName", false),
+    new Quote(2,  'Two things are infinite: the universe and human stupidity; and Im not sure about the universe.', "Albert Einstein", "bananaGuy", false ),
+    new Quote(3,  'Be the change that you wish to see in the world', "Mahatma Gandhi", "whiskers" ,false ),
+    new Quote(4,  'Go to heaven for the climate and hell for the company.', " Benjamin Franklin Wade", "humbleMan" , false)
   ]
 
 
